a2f35e9cf1b1aa99d45cf1fae767fbf7570ecafd
huangziang <huangziang_2024@qq.com>
2026-06-07 21:52:39 +0800

    feat: add Debug/Release build type support to scripts
    - Add BUILD_TYPE parameter to build_pure and build_run scripts
    - Add --run command for executing node without rebuild
    - Update radar wrapper with build type documentation

--- script/build_pure .sh
+++ script/build_pure .sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+BUILD_TYPE=${1:-Release}
 cd /workspace/ros_ws
 source /opt/ros/humble/setup.bash
-rm -rf build/radar_localization_lidar
-colcon build --packages-select radar_localization_lidar
+rm -rf build/radar_localization_lidar install/radar_localization_lidar
+echo "[PURE BUILD] Build type: ${BUILD_TYPE}"
+colcon build --packages-select radar_localization_lidar --cmake-args -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
