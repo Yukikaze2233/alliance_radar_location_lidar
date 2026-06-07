fe63e29a9e67725330829723777921d698de520f
huangziang <huangziang_2024@qq.com>
2026-06-08 00:45:56 +0800

    feat: add pointcloud capture module and rename capturer to image_capturer
    - Add PointCloudCapture node for subscribing to LiDAR topics
    - Replace generic capturer.hpp with image_capturer.cpp
    - Populate pointcloud_process.hpp with required includes

---
+++ ros_ws/src/radar_localization_lidar/src/pointcloud_capture.cpp
@@ -0,0 +1,17 @@
+#include "pointcloud_capture.hpp"
+#include <chrono>
+#include <mutex>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <thread>
+using namespace Radar::capture;
+class PointCloudCapture::Impl : public rclcpp::Node {
+public:
+    std::mutex mutex_;
+    PointCloudData latest_pointcloud_;
+    Impl(std::string pointcloud_topic)
+        : Node("pointcloud_capture_node") {
+        subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(pointcloud_topic,
+            10, std::bind(&Impl::PointCloudCallback, this, std::placeholders::_1));
+    }
+}
